8cc9f82dfb61321e94dabee82247abb1d99b1f0e
Bjoern20 <80956368+Bjoern20@users.noreply.github.com>
2021-04-03 12:49:09 +0000

    Deploying to gh-pages from master @ fb52586a231311bc94a28b7083f0e3e6bdabd89c 🚀

---
+++ workadventure_2/floortileset.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="floortileset" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="floortileset.png" width="256" height="256"/>
+ <tile id="20">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="46">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="collides" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
